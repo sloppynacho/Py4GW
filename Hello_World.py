821ff8b0726aac640662c14992e4584a057e09aa
apoguita <77995928+apoguita@users.noreply.github.com>
2025-04-25 07:04:39 -0600

    extending Keyhandler Class for Special Keys

--- Hello_World.py
+++ Hello_World.py
@@ -8,8 +8,16 @@ def main():
     global capture_mouse, mouse_x, mouse_y, pos_z
     
     if PyImGui.begin("timer test"):
-        if PyImGui.button("logout"):
-            Player.LogoutToCharacterSelect()
+        if PyImGui.button("right"):
+            Keystroke.Press(Key.RightArrow.value)
+            
+        if PyImGui.button("release right"):
+            Keystroke.Release(Key.RightArrow.value)
+            
+        if PyImGui.button("A"):
+            Keystroke.Press(Key.A.value)
+        if PyImGui.button("Release A"):
+            Keystroke.Release(Key.A.value)
             
         
     PyImGui.end()
