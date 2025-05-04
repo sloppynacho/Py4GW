114715273c2882a5a700520dc1c1b344a399ddea
Zilvereyes <zilver.muhlig.hansen@gmail.com>
2025-05-04 17:24:02 +0200

    Update Pet Helper.py
    Added the ability to see if Pet Helper is temporarily disabled (yellow text)
    Added Log toggle button (Default off)
    Fixed the Target Names on consol messages
    Changed a few hard coded values to use Enum instead

--- Widgets/Pet Helper.py
+++ Widgets/Pet Helper.py
@@ -19,6 +19,7 @@ class Global_Vars:
         self.title_frame_visible = False
         
         self.widget_active = True
+        self.log_action = False
         self.throttle_timer = ThrottledTimer(100)
         self.update_target_throttle_timer = ThrottledTimer(1000)
         
@@ -87,16 +88,20 @@ class Global_Vars:
         if self.player_name == "":
             self.player_name = self.agent_array.get_name(self.player_agent_id)
         self.party_target_name = self.agent_array.get_name(self.party_target_id)
-        self.owner_target_name = self.agent_array.get_name(self.party_target_id)
+        self.owner_target_name = self.agent_array.get_name(self.owner_target_id)
 
 global_vars = Global_Vars()
 
 def DrawWindow():
     global global_vars
     caption = "Helper ON" if global_vars.widget_active else "Helper OFF"
-    caption_color = Color(0, 255, 0,255).to_tuple() if global_vars.widget_active else Color(255, 0, 0,255).to_tuple()
-    if ImGui.floating_button(caption, global_vars.title_frame_coords.left+75, global_vars.title_frame_coords.top+3,100,30,caption_color):
+    caption_color = Color(0, 255, 0, 255).to_tuple() if global_vars.widget_active and global_vars.pet_bahavior != PetBehavior.Heel else Color(243, 230, 0, 255).to_tuple() if global_vars.widget_active and global_vars.pet_bahavior == PetBehavior.Heel else Color(255, 0, 0, 255).to_tuple()
+    log_caption_color = Color(0, 255, 0, 255).to_tuple() if global_vars.log_action else Color(255, 0, 0, 255).to_tuple()
+    if ImGui.floating_button(caption, global_vars.title_frame_coords.left+75, global_vars.title_frame_coords.top+3, 90, 30, caption_color):
         global_vars.widget_active = not global_vars.widget_active
+    if ImGui.floating_button("Log", global_vars.title_frame_coords.left+135, global_vars.title_frame_coords.top+3, 50, 30, log_caption_color):
+        global_vars.log_action = not global_vars.log_action
+    
 
 def configure():
     pass
@@ -130,17 +135,20 @@ def main():
         return
     
     # Set Party Target to Pet
-    if global_vars.party_target_id != 0 and global_vars.party_target_id != global_vars.pet_target_id and (global_vars.pet_bahavior == 1 or global_vars.pet_bahavior == 0):
-        ActionQueueManager().AddAction("ACTION", Party.Pets.SetPetBehavior,PetBehavior.Fight, global_vars.party_target_id)
-        Py4GW.Console.Log(module_name, f"{global_vars.pet_name} Fight {global_vars.party_target_name}({global_vars.party_target_id})", Py4GW.Console.MessageType.Info)
+    if global_vars.party_target_id != 0 and global_vars.party_target_id != global_vars.pet_target_id and (global_vars.pet_bahavior == PetBehavior.Guard or global_vars.pet_bahavior == PetBehavior.Fight):
+        ActionQueueManager().AddAction("ACTION", Party.Pets.SetPetBehavior, PetBehavior.Fight, global_vars.party_target_id)
+        if global_vars.log_action:
+            Py4GW.Console.Log(module_name, f"{global_vars.pet_name} Fight {global_vars.party_target_name} ({global_vars.party_target_id})", Py4GW.Console.MessageType.Info)
         global_vars.update_target_throttle_timer.Reset()
-    elif global_vars.owner_target_id != 0 and global_vars.owner_target_id != global_vars.pet_target_id and (global_vars.pet_bahavior == 1 or global_vars.pet_bahavior == 0):
-        ActionQueueManager().AddAction("ACTION", Party.Pets.SetPetBehavior,PetBehavior.Fight, global_vars.owner_target_id)
-        Py4GW.Console.Log(module_name, f"{global_vars.pet_name} Fight {global_vars.owner_target_name}({global_vars.owner_target_id})", Py4GW.Console.MessageType.Info)
+    elif global_vars.owner_target_id != 0 and global_vars.owner_target_id != global_vars.pet_target_id and (global_vars.pet_bahavior == PetBehavior.Guard or global_vars.pet_bahavior == PetBehavior.Fight):
+        ActionQueueManager().AddAction("ACTION", Party.Pets.SetPetBehavior, PetBehavior.Fight, global_vars.owner_target_id)
+        if global_vars.log_action:
+            Py4GW.Console.Log(module_name, f"{global_vars.pet_name} Fight {global_vars.owner_target_name} ({global_vars.owner_target_id})", Py4GW.Console.MessageType.Info)
         global_vars.update_target_throttle_timer.Reset()
-    elif global_vars.party_target_id == 0 and global_vars.owner_target_id == 0 and global_vars.pet_bahavior == 0:
-        ActionQueueManager().AddAction("ACTION", Party.Pets.SetPetBehavior,PetBehavior.Guard, global_vars.player_agent_id)
-        Py4GW.Console.Log(module_name, f"{global_vars.pet_name} Guard {global_vars.player_name}", Py4GW.Console.MessageType.Info)
+    elif global_vars.party_target_id == 0 and global_vars.owner_target_id == 0 and global_vars.pet_bahavior == PetBehavior.Fight:
+        ActionQueueManager().AddAction("ACTION", Party.Pets.SetPetBehavior, PetBehavior.Guard, global_vars.player_agent_id)
+        if global_vars.log_action:
+            Py4GW.Console.Log(module_name, f"{global_vars.pet_name} Guard {global_vars.player_name}", Py4GW.Console.MessageType.Info)
         global_vars.update_target_throttle_timer.Reset()
     
     
