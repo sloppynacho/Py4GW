54d76e592c037e294a2bb5c83ee9087503a8395b
AC <36128680+Ac108a@users.noreply.github.com>
2025-05-10 18:56:51 +0200

    Create __init__.py
    Package import

---
+++ Blessed_helpers/__init__.py
@@ -0,0 +1,9 @@
+from .Blessing_Core          import BlessingRunner, FLAG_DIR
+from .Verify_Blessing        import has_any_blessing
+
+__all__ = [
+    "BlessingRunner",
+    "FLAG_DIR",
+    "has_any_blessing",
+    "BlessingRunner",
+]
