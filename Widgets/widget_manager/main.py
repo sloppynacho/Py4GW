1c6e0b6354bd78ac88da122fae999c53cab9a2e3
Torx10 <torx10h@gmail.com>
2025-05-17 08:26:06 +1000

    perf(widget_manager)
    Reduced function calls in main, slight throttle on ini writing in old widget manager.

--- Widgets/widget_manager/main.py
+++ Widgets/widget_manager/main.py
@@ -1,4 +1,4 @@
-from Py4GWCoreLib import Py4GW, Map, Party, ConsoleLog, UIManager, time
+from Py4GWCoreLib import Py4GW, Map, Party, ConsoleLog
 import traceback
 import sys
 
@@ -14,13 +14,13 @@ from .config_scope import is_in_character_select, character_select
 account_init = True
 run_once = False
 checked = False
-
-
 def main():
     global account_init, run_once, checked, character_select
     
     try:
-       
+        is_loaded = Party.IsPartyLoaded()
+        is_loading = Map.IsMapLoading()
+
         if not run_once:
             ConsoleLog("WidgetHandler", "Initialize Character Selection", Py4GW.Console.MessageType.Info)
             character_select = is_in_character_select()
@@ -30,36 +30,35 @@ def main():
             handler.discover_widgets()
             state.initialized = True
 
-        if not Party.IsPartyLoaded() and Map.IsMapLoading() and not checked:
+        if not is_loaded and is_loading and not checked:
             character_select = is_in_character_select()
             ConsoleLog("WidgetHandler", f"Check for Character Selection: {character_select}", Py4GW.Console.MessageType.Info)
             checked = True
-        
-        if Party.IsPartyLoaded() and not Map.IsMapLoading() and checked:
+
+        if is_loaded and not is_loading and checked:
             character_select = is_in_character_select()
             ConsoleLog("WidgetHandler", f"Check for Character Selection: {character_select}", Py4GW.Console.MessageType.Info)
             checked = False 
 
-        if (account_init and not Map.IsMapLoading() and Party.IsPartyLoaded()):
+        if account_init and is_loaded and not is_loading:
             account_init = False
             ConsoleLog("WidgetHandler", "Initialize Account Settings", Py4GW.Console.MessageType.Info)
             handler._initialize_account_settings()
             handler._initialize_account_config()
             initialize_settings()
-            
+
         if state.old_menu:
             draw_old_widget_ui()
-        
+
         if state.enable_all:
             handler.execute_enabled_widgets()
             handler.execute_configuring_widgets()
-            
+
         if state.enable_quick_dock:
             quick_dock_menu()
+
         draw_floating_menu()
         draw_embedded_widget_config()
-
-        
 
     except Exception as e:
         err_type = type(e).__name__
